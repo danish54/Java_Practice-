99a273628394fd2ddbc05e7b89c8317ec064cf82
Danish Ali <v-danishali@microsoft.com>
2022-10-16 18:00:05 +0530

    PrintFibonacciSeries

---
+++ src/fibonacci_Series/Fibonacce_Series_forloop.java
@@ -0,0 +1,26 @@
+package fibonacci_Series;
+
+public class Fibonacce_Series_forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		printFibonacciSeries(10);
+		
+		}
+	
+	public static void  printFibonacciSeries(int count) {
+		int a = 0;
+		int b = 1;
+		int c = 1;
+		for (int i = 1; i <= count; i++) {
+			System.out.print(a + ", ");
+			a = b;
+			b = c;
+			c = a + b;
+		}
+
+	}
+
+}
